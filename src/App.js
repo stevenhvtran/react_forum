0b0c15b13dd1ee5a47a1d50d26d3cdfbf57a85cc
stevenhvtran <stransteven@outlook.com>
2018-07-01 18:53:27 +1000

    Added Component to get  a post

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Fetch } from "react-request";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Some new title</h1>
-        </header>
-        <p className="App-intro">
-          Here is <code>some random code</code>
-        </p>
-      </div>
+      <body>
+        <h1 className="title">REACT FORUM</h1>
+        <Post post_id="1" />
+      </body>
+    );
+  }
+}
+
+class Post extends Component {
+  render() {
+    return (
+      <Fetch
+        url={
+          "https://flask-forum-api.herokuapp.com/api/post/" + this.props.post_id
+        }
+        method="GET"
+      >
+        {({ fetching, failed, data }) => {
+          if (fetching) {
+            return <div>Loading data...</div>;
+          }
+
+          if (failed) {
+            return <div>The request did not succeed.</div>;
+          }
+
+          if (data) {
+            return (
+              <div>
+                <div>Post Title: {data.title}</div>
+              </div>
+            );
+          }
+
+          return null;
+        }}
+      </Fetch>
     );
   }
 }
