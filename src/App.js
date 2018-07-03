9ea2ff01678ab41087f5493fda6d9f0dfeba71fe
stevenhvtran <stransteven@outlook.com>
2018-07-04 09:36:44 +1000

    Before rewrite
    Rewrite aims to remove react-request dependency as it has bad integration
    with Components

--- src/App.js
+++ src/App.js
@@ -8,8 +8,107 @@ class App extends Component {
     return (
       <body>
         <h1 className="title">REACT FORUM</h1>
-        <PostsWrapper/>
+        <PostsWrapper />
       </body>
+    );
+  }
+}
+
+class Login extends Component {
+  handleLogin(e) {
+    e.preventDefault();
+    this.props.view
+  }
+  render() {
+    return (
+      <Fetch
+        url={"https://flask-forum-api.herokuapp.com/"}
+        method="GET"
+        headers={{
+          Accept: "application/json",
+          Authorization:
+            "Basic " + btoa(this.props.username + ":" + this.props.password)
+        }}
+        cacheResponse={false}
+        lazy={false}
+      >
+        {({ fetching, failed, data }) => {
+          if (fetching) {
+            return <img src={loading} alt="loading" />;
+          }
+
+          if (failed) {
+            return <strong>Login failed</strong>;
+          }
+
+          if (data) {
+            return <strong onLoad={this.handleLogin.bind(this)}>Login success!</strong>;
+          }
+        }}
+      </Fetch>
+    );
+  }
+}
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      showFetch: false
+    };
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleUserChange(event) {
+    this.setState({ username: event.target.value });
+    this.setState({ showFetch: false });
+  }
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+    this.setState({ showFetch: false });
+  }
+
+  handleClick() {
+    this.setState({ showFetch: true });
+  }
+
+  render() {
+    return (
+      <div className="register">
+        <strong>
+          {this.state.showFetch ? (
+            <Login
+              username={this.state.username}
+              password={this.state.password}
+            />
+          ) : (
+            <strong>Login</strong>
+          )}
+        </strong>
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.handleUserChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+          <button type="button" onClick={this.handleClick}>
+            Login
+          </button>
+        </form>
+      </div>
     );
   }
 }
@@ -20,7 +119,6 @@ class RegisterForm extends Component {
     this.state = {
       username: "",
       password: "",
-      message: "something",
       showFetch: false
     };
     this.handleUserChange = this.handleUserChange.bind(this);
@@ -113,10 +211,31 @@ class Register extends Component {
 }
 
 class PostsWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: "login",
+      credentials: null
+    };
+  }
+
+  changeView(creds) {
+    this.setState({
+      view: "project",
+      // credentials: creds
+    });
+  }
+
   render() {
     return (
       <div className="post-wrapper">
-        <RegisterForm/>
+        {this.state.view}
+        {this.state.view === "login" ? (
+          <div>
+            <RegisterForm />
+            <LoginForm view={this.changeView.bind(this)}/>
+          </div>
+        ) : null}
         <Fetch url="https://flask-forum-api.herokuapp.com/api/posts">
           {({ fetching, failed, data }) => {
             if (fetching) {
@@ -128,8 +247,7 @@ class PostsWrapper extends Component {
             }
 
             if (data) {
-              var postsElements = data.posts;
-              return postsElements.map(post => <Post post_id={post.post_id} />);
+              return data.posts.map(post => <Post post_id={post.post_id} />);
             }
 
             return null;
@@ -148,33 +266,31 @@ class Post extends Component {
 
   render() {
     return (
-      <Fetch url={this.baseApiUrl + this.props.post_id}>
-        {({ fetching, failed, data }) => {
-          if (fetching) {
-            return (
-              <div className="post">
-                <img className="center" src={loading} alt="loading" />
-              </div>
-            );
-          }
+      <div className="post">
+        <Fetch url={this.baseApiUrl + this.props.post_id}>
+          {({ fetching, failed, data }) => {
+            if (fetching) {
+              return <img className="center" src={loading} alt="loading" />;
+            }
 
-          if (failed) {
-            return <div className="post">The request did not succeed.</div>;
-          }
+            if (failed) {
+              return "The request did not succeed.";
+            }
 
-          if (data) {
-            return (
-              <div className="post">
-                <div className="post-title">{data.title}</div>
-                <div className="post-body">{data.body}</div>
-                <div className="post-author">by {data.author_name}</div>
-              </div>
-            );
-          }
+            if (data) {
+              return (
+                <div>
+                  <div className="post-title">{data.title}</div>
+                  <div className="post-body">{data.body}</div>
+                  <div className="post-author">by {data.author_name}</div>
+                </div>
+              );
+            }
 
-          return null;
-        }}
-      </Fetch>
+            return null;
+          }}
+        </Fetch>
+      </div>
     );
   }
 }
